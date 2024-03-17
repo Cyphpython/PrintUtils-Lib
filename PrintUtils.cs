88b2f1536b62282711ef07a185c364f96e2bf29c
Elden Modder <90405810+Cyphpython@users.noreply.github.com>
2024-03-17 17:50:42 +0100

    Add files via upload

---
+++ PrintUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace PrintUtils
+{
+    public static class PrintUtils
+    {
+        public static void SlowPrint(string text, int rate = 20)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(1000/rate);
+            }
+            Console.WriteLine();
+        }
+    }
+}
